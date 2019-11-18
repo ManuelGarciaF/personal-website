dd3e8a1ab037b515099039a775556f0d8d5d99c4
ManuelGarciaF <mgarciafrigo@gmail.com>
2019-11-18 01:02:40 -0300

    Updates

---
+++ precache-manifest.17e86929ba9c59859b30f2378007fc61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40168d02999da71ceb0c3efcb903dbfd",
+    "url": "manuelgarciaf.xyz/index.html"
+  },
+  {
+    "revision": "96a6633563d561e72cec",
+    "url": "manuelgarciaf.xyz/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d94be223897956701708",
+    "url": "manuelgarciaf.xyz/static/js/2.033ff4dc.chunk.js"
+  },
+  {
+    "revision": "96a6633563d561e72cec",
+    "url": "manuelgarciaf.xyz/static/js/main.79d3e826.chunk.js"
+  },
+  {
+    "revision": "dcc526566b58eca7a4d0",
+    "url": "manuelgarciaf.xyz/static/js/runtime-main.16a47caf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "manuelgarciaf.xyz/static/media/logo.25bf045c.svg"
+  }
+]);
