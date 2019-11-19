3e16c2c785ae557544e537f24333d7018acb4cf9
ManuelGarciaF <mgarciafrigo@gmail.com>
2019-11-19 18:54:04 -0300

    add font to css

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1 style={h1Style}>TODO</h1>
+        <h1>TODO</h1>
       </div>
     )
   }
-}
-
-const h1Style = {
-  textAlign: 'center',
-  fontSize: '10rem',
-  margin: 'auto'
 }
 
 export default App
